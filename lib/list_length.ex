33b4b3dca97050a365ba5ef6281af30eb2ec8501
Jefferson Farias <jefferson.farias7@gmail.com>
2021-11-02 11:11:10 -0300

    count elements of a list

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  @spec call(list()) :: number() | :error
+  def call(list) when is_list(list) do
+    count(list, 0)
+  end
+
+  def call(_list), do: :error
+
+  def count([], count), do: count
+
+  def count([_head | tail], count) do
+    count(tail, count + 1)
   end
 end
